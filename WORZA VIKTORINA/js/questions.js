3ab75c1eb6801f085f6a06b18b3f7685c3265bba
Otabek <145839643+otabek2009anvarjonov@users.noreply.github.com>
2023-11-02 15:14:24 +0500

    Add files via upload

---
+++ WORZA VIKTORINA/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "O'zbekistonning poytaxti?",
+        answer: "Toshkent",
+        options: [
+            "Baku",
+            "Toshkent",
+            "Farg'ona",
+            "Madrid"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Taekwondo bo'yicha oxirgi olimpiada chempioni..",
+        answer: "Ulug'bek Rashitov",
+        options: [
+            "Dmitriy Shoking",
+            "Nikita Rafalovich",
+            "Ulug'bek Rashitov",
+            "Hechkim"
+        ]
+    },
+    {
+        numb: 3,
+        question: "HTML ma'nosi qanday?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Trainer Marking Language",
+            "Hyper Text Markup Leveler",
+            "Hyper Text Marketing Language",
+            "Hyper Text Markup Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "JavaScriptda cinsole.log nima?",
+        answer: "Terminal",
+        options: [
+            "link",
+            "p",
+            "script",
+            "Terminal"
+        ]
+    },
+    {
+        numb: 5,
+        question: "# belgisi qanaqa selektor ekanligini bildiradi?",
+        answer: "id",
+        options: [
+            "html",
+            "tag",
+            "class",
+            "id"
+        ]
+    },
+    {
+        numb: 6,
+        question: "O'zbekistonning viloyatlaari nechta?",
+        answer: "12",
+        options: [
+            "12",
+            "14",
+            "18",
+            "bilmiman"
+        ]
+    },
+    {
+        numb: 7,
+        question: "O'qituvchimizning ismi",
+        answer: "Muhiddin aka",
+        options: [
+            "Muhiddin aka",
+            "Nodirbek aka",
+            "Nurbek aka",
+            "Hechkim"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Managerimiz ismi?",
+        answer: "Nodirbek aka",
+        options: [
+            "Muhiddin aka",
+            "Nodirbek aka",
+            "Nurbek aka",
+            "Hechkim"
+        ]
+    },
+    {
+        numb: 9,
+        question: "CSS fayl HTMLda qanday ulanadi?",
+        answer: "link",
+        options: [
+            "link",
+            "p",
+            "script",
+            "div"
+        ]
+    },
+    {
+        numb: 10,
+        question: ". belgisi qanaqa selektor ekanligini bildiradi?",
+        answer: "class",
+        options: [
+            "html",
+            "tag",
+            "class",
+            "id"
+        ]
+    },
+];
